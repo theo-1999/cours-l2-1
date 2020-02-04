84fa86b49d7926f72ba5c77619f71ba089e3654b
Ubuntu <ubuntu@ip-172-31-89-193.ec2.internal>
2020-02-04 14:18:20 +0000

    message

--- javascript/exercices.js
+++ javascript/exercices.js
@@ -1,4 +1,28 @@
 const anagrammes = (stringA, stringB) => {
+  //replace permet de garder juste les élément qui suive
+  stringA=stringA.replace(/[^a-zA-Z]/g,'');
+  stringB=stringB.replace(/[^a-zA-Z]/g,'');
+  
+  stringA=stringA.toUpperCase();
+  stringB=stringB.toUpperCase();
+
+  let tabA = stringA.split("");
+  let tabB =stringB.split("");
+
+  //Mettre dans l'ordre alphabétique
+  tabA = tabA.sort();
+  tabB = tabB.sort();
+
+  let x = true;
+
+  tabA.forEach((el, indice) => {
+    if(tabA[indice]!=tabB[indice]){
+      //console.log(tabA[indice],tabB[indice])
+      x = false;
+    }
+  })
+  return x;
+
   /**
    * stringA est égale à stringB si et seulement s'ils partagent les mêmes
    * caractères alphabétiques dans la même quantité.
@@ -10,11 +34,35 @@ const anagrammes = (stringA, stringB) => {
    * anagrams('RAIL! SAFETY!', 'fairy tales') === true
    * anagrams('Hi there', 'Bye there') === false
    */
-
+//console.log(tabA);
 };
+
+//anagrammes("test", "estt")
+
 
 
 class Stack {
+
+  constructor(){
+    this.tab = [];
+  }
+
+
+  push(val) {
+    this.tab[this.tab.length] = val;
+  }
+
+  pop(){
+    let a = this.tab[this.tab.length-1];
+    this.tab.splice(this.tab.length-1, 1);
+    return a;
+  }
+
+  peek(){
+    return this.tab[0];
+  }
+
+
 /**
  * Créer une structure d'empilement. La structure doit être
  * une classe contenant les méthodes :
@@ -36,6 +84,32 @@ class Stack {
 
 
 const fizzBuzz = (n) => {
+    
+    
+    let x = 1
+    let y 
+  while (n>0){
+    
+    y = x
+    if(x%3 == 0){
+      y="fizz"
+      
+    }
+    if(x%5 == 0){
+      y="buzz"
+    }
+    if(x%3 == 0 && x%5 == 0){
+      y="fizzbuzz"
+    }
+    
+    x++
+     console.log(y)
+     n--;
+    }
+    
+ 
+  
+
 /**
  * Affiche les nombres de 1 à n, en remplaçant les multiples de 3 par fizz et 
  * les multiples de 5 par buzz
@@ -70,8 +144,46 @@ const spirale = (n) => {
  *              [11, 16, 15, 6],
  *              [10,  9,  8, 7]]
  */
+ 
+    let result = new Array(n).fill().map(() => new Array(n).fill('')); // create empty n x n array
+    let counter = 1;
+    let startCol = 0;
+    let endCol = n - 1;
+    let startRow = 0;
+    let endRow = n - 1;
+    while (startCol <= endCol && startRow <= endRow) {
+        for (let i = startCol; i <= endCol; i++) {
+            result[startRow][i] = counter;
+            counter++;
+        }
+        startRow++;
+        for (let j = startRow; j <= endRow; j++) {
+            result[j][endCol] = counter;
+            counter++;
+        }
 
-};
+        endCol--;
+
+        for (let i = endCol; i >= startCol; i--) {
+            result[endRow][i] = counter;
+            counter++;
+        }
+
+        endRow--;
+        for (let i = endRow; i >= startRow; i--) {
+            result[i][startCol] = counter;
+            counter++;
+        }
+
+        startCol++;
+
+    }
+
+    return result;
+
+}
+
+
 
 
 const puissance4 = (grid) => {
